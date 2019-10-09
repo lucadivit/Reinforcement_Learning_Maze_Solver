ed7e0425126abf63972a48c6e95b42b1b8e98e75
Luca Di Vita <lucadivita.ldv@gmail.com>
2019-10-09 13:37:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ boltzmann = BoltzmannPolicy()
 boltzmann_e_greedy = BoltzmannEGreedyPolicy()
 
 def train_ql_agent():
-    qLearningAgent = QLearningAgent(env=env, policy=e_greedy, number_of_episodes=5)
-    qLearningAgent.start_training(time_between_step=0.1, time_between_episode=1)
+    q_table_file_name = "q_learning_table.pkl"
+    qLearningAgent = QLearningAgent(env=env, policy=e_greedy)
+    qLearningAgent.load_stored_q_table(q_table_file_name)
+    qLearningAgent.start_training(num_of_episodes=5, time_between_step=0.1, time_between_episode=1, save_q_table=True, q_table_file_name=q_table_file_name)
 
 def train_sarsa_agent():
-    sarsaAgent = SarsaAgent(env=env, policy=e_greedy, number_of_episodes=5)
-    sarsaAgent.start_training(time_between_step=0.1, time_between_episode=1)
+    sarsa_file_name = "sarsa_table.pkl"
+    sarsaAgent = SarsaAgent(env=env, policy=e_greedy)
+    sarsaAgent.load_stored_q_table(sarsa_file_name)
+    sarsaAgent.start_training(num_of_episodes=5, time_between_step=0.1, time_between_episode=1, save_q_table=True, q_table_file_name=sarsa_file_name)
 
 def main():
     train_ql_agent()
